ceec8577b52bd826426b0295e335dadc96551333
justyna06 <s33127@pjwstk.edu.pl>
2025-03-19 18:16:55 +0100

    zadanie1

--- main.java
+++ main.java
@@ -1,2 +1,20 @@
 public class main {
+    public static int[] filterEvenIndexOddValue(int[] array){
+
+        int counter = 0;
+        for(int i = 0; i < array.length; i += 2){
+            if(array[i] % 2 == 0){
+                counter++;
+            }
+        }
+        int[] result = new int[counter];
+        int index = 0;
+
+        for(int i = 0; i < array.length; i += 2){
+            if(array[i] % 2 == 0){
+                result[index++] = array[i];
+            }
+        }
+        return result;
+    }
 }
